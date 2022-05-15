1910d21380a942edba59a4d26f32c02372abd810
Matt Doren <mattdoren62@gmail.com>
2022-05-15 12:45:21 -0500

    Create DrMarioDrawables.kt

---
+++ app/src/main/java/com/example/mad155final/DrMarioDrawables.kt
@@ -0,0 +1,9 @@
+package com.example.mad155final
+
+data class DrMarioDrawables(
+    val dr0: Int = R.drawable.dr0,
+    val dr1: Int = R.drawable.dr1,
+    val dr2: Int = R.drawable.dr2,
+    val dr3: Int = R.drawable.dr3,
+    val dr4: Int = R.drawable.dr4
+)
