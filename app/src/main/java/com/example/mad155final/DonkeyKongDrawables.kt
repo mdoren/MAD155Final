10723257da335f5a5e4bace5ba6470bf2504cff7
Matt Doren <mattdoren62@gmail.com>
2022-05-15 12:44:45 -0500

    Create DonkeyKongDrawables.kt

---
+++ app/src/main/java/com/example/mad155final/DonkeyKongDrawables.kt
@@ -0,0 +1,8 @@
+package com.example.mad155final
+
+data class DonkeyKongDrawables(
+    val dk0: Int = R.drawable.dk0,
+    val dk1: Int = R.drawable.dk1,
+    val dk2: Int = R.drawable.dk2,
+    val dk3: Int = R.drawable.dk3
+)
