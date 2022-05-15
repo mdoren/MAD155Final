d79ef2de78005a89c0795a929eb076f0e07570b2
Matt Doren <mattdoren62@gmail.com>
2022-05-15 12:43:51 -0500

    Create CaptainFalconDrawables.kt

---
+++ app/src/main/java/com/example/mad155final/CaptainFalconDrawables.kt
@@ -0,0 +1,10 @@
+package com.example.mad155final
+
+data class CaptainFalconDrawables(
+    val captain0: Int = R.drawable.captain0,
+    val captain1: Int = R.drawable.captain1,
+    val captain2: Int = R.drawable.captain2,
+    val captain3: Int = R.drawable.captain3,
+    val captain4: Int = R.drawable.captain4,
+    val captain5: Int = R.drawable.captain5
+)
