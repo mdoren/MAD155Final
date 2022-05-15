c175949eec865384071dbd57e62dc723d2ae546c
Matt Doren <mattdoren62@gmail.com>
2022-05-15 12:43:32 -0500

    Create BowserDrawables.kt

---
+++ app/src/main/java/com/example/mad155final/BowserDrawables.kt
@@ -0,0 +1,8 @@
+package com.example.mad155final
+
+data class BowserDrawables(
+    val bowser0: Int = R.drawable.bowser0,
+    val bowser1: Int = R.drawable.bowser1,
+    val bowser2: Int = R.drawable.bowser2,
+    val bowser3: Int = R.drawable.bowser3
+        )
